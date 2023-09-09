e475ff654989b062e31a76dcc65a22129538385e
NaokiTakahashi <naoki.takahashi060@gmail.com>
2023-09-09 14:04:49 +0900

    Support galactic

--- scanmatcher/include/scanmatcher/scanmatcher_component.h
+++ scanmatcher/include/scanmatcher/scanmatcher_component.h
@@ -44,9 +44,9 @@ extern "C" {
 #include <tf2_ros/buffer.h>
 #include <tf2_ros/transform_listener.h>
 #include <tf2_ros/transform_broadcaster.h>
-#include <tf2_sensor_msgs/tf2_sensor_msgs.hpp>
-#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
-#include <tf2_eigen/tf2_eigen.hpp>
+#include <tf2_sensor_msgs/tf2_sensor_msgs.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#include <tf2_eigen/tf2_eigen.h>
 
 #include <sensor_msgs/msg/point_cloud2.hpp>
 #include <sensor_msgs/msg/imu.hpp>
